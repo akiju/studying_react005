7a072ab160adac9b2e6e768b428629e4399a27e8
akiju <99388105+akiju@users.noreply.github.com>
2022-03-22 18:46:53 +0900

    Initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,8 @@
+
+export const Home = () => {
+  return(
+    <div>
+      <h1>Homeパージだよ。</h1>
+    </div>
+  )
+}
---
+++ src/Page2.jsx
@@ -0,0 +1,7 @@
+export const Page2 = () => {
+  return (
+    <div>
+      <h1>Page2パージだよ。</h1>
+    </div>
+  );
+};
