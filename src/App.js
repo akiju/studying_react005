1739e4815f8d59284cf7505004a98937e1c13f6d
akiju <99388105+akiju@users.noreply.github.com>
2022-03-23 19:45:27 +0900

    220323 220323

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page1A } from "./Page1A";
+import { Page1B } from "./Page1B";
+import { Page2 } from "./Page2";
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/Page1">page1</Link>
+        <br />
+        <Link to="/Page2">page2</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route exact path="/Page1" element={<Page1 />}>
+          <Route exact path="d1A" element={<Page1A />} />
+          <Route exact path="d1B" element={<Page1B />} />
+        </Route>
+        <Route path="/Page2" element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,13 @@
+import { Link, Outlet } from "react-router-dom";
+
 export const Page1 = () => {
   return (
     <div>
       <h1>Page1パージだよ。</h1>
+      <Outlet />
+      <Link to="d1A">DetailA</Link>
+      <br />
+      <Link to="d1B">DetailB</Link>
     </div>
   );
 };
